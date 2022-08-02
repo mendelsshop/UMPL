2beaac3c1d31f71757d4fe36d95851845178bafa
mendelsshop <runner@fv-az399-63.4urtqdmtpd2erju50p4tzyn2ng.bx.internal.cloudapp.net>
2022-08-02 09:14:03 +0000

    cargo formatted: shortened lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
 #![warn(clippy::pedantic, clippy::nursery, clippy::cargo)]
 #![deny(clippy::use_self, rust_2018_idioms)]
-#![allow(clippy::missing_panics_doc, clippy::must_use_candidate, clippy::case_sensitive_file_extension_comparisons, clippy::match_wildcard_for_single_variants, clippy::cast_possible_truncation, clippy::cast_sign_loss, clippy::cognitive_complexity, clippy::float_cmp)]
+#![allow(
+    clippy::missing_panics_doc,
+    clippy::must_use_candidate,
+    clippy::case_sensitive_file_extension_comparisons,
+    clippy::match_wildcard_for_single_variants,
+    clippy::cast_possible_truncation,
+    clippy::cast_sign_loss,
+    clippy::cognitive_complexity,
+    clippy::float_cmp
+)]
 pub mod cli;
 pub mod error;
 pub mod eval;
