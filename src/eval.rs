d878229c3d41791e67bdcb07d850cc54d27d122b
mendelsshop <mendelsshop@gmail.com>
2023-03-23 13:59:15 -0400

    [eval] formated last commit

--- src/eval.rs
+++ src/eval.rs
@@ -582,9 +582,9 @@ impl<'a> Eval<'a> {
                     error(expr.info, "return statement outside of function");
                 }
                 // implicit return
-                _ if idx == len - 1 && self.in_function  =>{
+                _ if idx == len - 1 && self.in_function => {
                     // if the last expression is not a return statement then we return the last expression
-                        return Some(Stopper::Return(expr));
+                    return Some(Stopper::Return(expr));
                 }
                 _ => {} // we should never find functions in the outer scope, as those are found in the find_functions function
 
